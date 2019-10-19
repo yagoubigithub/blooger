4d2e7000f7fe4966cca19f38fb65f4ea7599b6be
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-10-19 14:43:57 +0100

    carousel

--- js/main.js
+++ js/main.js
@@ -9,4 +9,15 @@ $(document).ready(function(){
         $nav.toggleClass('collapse');
     })
 
+    // owl-carousel for blog
+
+    $('.owl-carousel').owlCarousel({
+        loop :true,
+        autoplay :false,
+        autoplayTimeout: 3000,
+        dots : false,
+        nav :true,
+        navText : [$('.owl-navigation  .owl-nav-prev'),$('.owl-navigation  .owl-nav-next')]
+    });
+
 })
