dd0942ef267da5ee33a19ac7bbbc0fe79a916d9c
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-10-20 16:41:18 +0200

    make me responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+    0:{
+        items :1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items : 3
+
+    }
+}
 $(document).ready(function(){
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse')
@@ -17,7 +32,8 @@ $(document).ready(function(){
         autoplayTimeout: 3000,
         dots : false,
         nav :true,
-        navText : [$('.owl-navigation  .owl-nav-prev'),$('.owl-navigation  .owl-nav-next')]
+        navText : [$('.owl-navigation  .owl-nav-prev'),$('.owl-navigation  .owl-nav-next')],
+        responsive :responsive
     });
 
     // click to scroll top
